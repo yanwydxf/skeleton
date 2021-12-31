6e8e7685127208acd958bf86e9e1a15e70c09ca5
离岛 <yanwydxf@163.com>
2021-12-31 14:41:09 +0800

    修复微信小程序显示问题

--- skeleton.vue
+++ skeleton.vue
@@ -3,12 +3,12 @@
 		<view class="skeleton" v-show="loading" :class="[animationClass]" v-for="skeleton in count">
 			<view class="row" >
 				<view v-for="(item,index) in row" :key="item">
-					<view :style="{width:rowWidth,height:rowHeight}" v-bind="$attrs" v-if="variant === 'image'" :class="[varianttype]">
+					<view :style="{width:rowWidth,height:rowHeight}" v-if="variant === 'image'" :class="[varianttype]">
 						<svg  viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
 						    <path d="M64 896V128h896v768H64z m64-128l192-192 116.352 116.352L640 448l256 307.2V192H128v576z m224-480a96 96 0 1 1-0.064 192.064A96 96 0 0 1 352 288z"/>
 						</svg>
 					</view>
-					<view v-else :class="[varianttype]" :style="{width:rowWidth,height:rowHeight}" v-bind="$attrs" class="row-class"></view>
+					<view v-else :class="[varianttype]" :style="{width:rowWidth,height:rowHeight}"  class="row-class"></view>
 				</view>
 			</view>
 		</view>
